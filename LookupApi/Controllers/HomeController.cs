894aae2f3af174f290e64c68c75e39b625ae448f
Lakshmikanth Balasundaram <blakshmikanth@gmail.com>
2022-06-13 11:31:53 +0100

    Bootcamp Projects. Added Lookup projects

---
+++ LookupApi/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using LookupApi.Models;
+using Twilio;
+using Twilio.Rest.Lookups.V1;
+
+namespace LookupApi.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(IConfiguration configuration, ILogger<HomeController> logger)
+    {
+        _configuration = configuration;
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index([FromForm] string phoneNumber)
+    {
+        ViewBag.phoneNumber = phoneNumber;
+        
+        TwilioClient.Init(_configuration.GetValue<string>("Twilio:AccountSid"), 
+            _configuration.GetValue<string>("Twilio:AuthToken"));
+        
+        var response = PhoneNumberResource.Fetch(countryCode: "GB",
+            pathPhoneNumber: new Twilio.Types.PhoneNumber(phoneNumber));
+
+        ViewBag.response = response;
+        return View();
+    }
+
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ LookupApi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LookupApi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
